9d4c86c531858497b4935ca80b2b64b7000b254d
Callum Cooper <ccooper1982@proton.me>
2025-12-24 00:37:26 +0000

    Added accounts wdidget

--- src/wali.cpp
+++ src/wali.cpp
@@ -12,6 +12,7 @@
 #include <Wt/WHBoxLayout.h>
 #include <Wt/WVBoxLayout.h>
 #include <Wt/WGridLayout.h>
+#include <Wt/WGroupBox.h>
 #include <Wt/WGlobal.h>
 #include <Wt/WLength.h>
 #include <Wt/WLineEdit.h>
@@ -242,6 +243,8 @@ public:
 
         m_home = layout->addWidget(make_wt<HomePartitionWidget>(std::bind_front(&PartitionsWidget::on_home_to_home, std::ref(*this))));
         m_home->create(parts, std::bind(&PartitionsWidget::validate_selection, std::ref(*this)));
+
+        // TODO bootloader
       }
     }
 
@@ -311,6 +314,47 @@ private:
   Wt::WCheckBox * m_copy_config;
 };
 
+class AccountWidget : public Wt::WContainerWidget
+{
+public:
+  AccountWidget ()
+  {
+    auto layout = setLayout(make_wt<Wt::WVBoxLayout>());
+
+    auto gb_root = layout->addWidget(make_wt<Wt::WGroupBox>("Root"));
+    auto gb_user = layout->addWidget(make_wt<Wt::WGroupBox>("User"));
+
+    auto root_layout = gb_root->setLayout(make_wt<Wt::WHBoxLayout>());
+    auto user_layout = gb_user->setLayout(make_wt<Wt::WGridLayout>());
+
+    root_layout->addWidget(make_wt<Wt::WText>("Password"));
+    m_root_password = root_layout->addWidget(make_wt<Wt::WLineEdit>("arch"));
+
+    root_layout->addStretch(1);
+
+    user_layout->addWidget(make_wt<Wt::WText>("Username"), 0, 0);
+    m_user_username = user_layout->addWidget(make_wt<Wt::WLineEdit>(), 0, 1);
+
+    user_layout->addWidget(make_wt<Wt::WText>("Password"), 1, 0);
+    m_user_password = user_layout->addWidget(make_wt<Wt::WLineEdit>(), 1, 1);
+
+    user_layout->addWidget(make_wt<Wt::WText>("Sudo"), 2, 0);
+    m_user_sudo = user_layout->addWidget(make_wt<Wt::WCheckBox>(), 2, 1);
+    m_user_sudo->setCheckState(Wt::CheckState::Checked);
+
+    // use as a stretch
+    user_layout->addWidget(make_wt<Wt::WText>(""),0,2);
+    user_layout->setColumnStretch(2, 1);
+
+    layout->addStretch(1);
+  }
+
+private:
+  Wt::WLineEdit * m_root_password,
+                * m_user_username,
+                * m_user_password;
+  Wt::WCheckBox * m_user_sudo;
+};
 
 class HelloApplication : public Wt::WApplication
 {
@@ -332,6 +376,7 @@ public:
     menu->addItem("Introduction", make_wt<IntroductionWidget>());
     menu->addItem("Partitions",   make_wt<PartitionsWidget>());
     menu->addItem("Network",      make_wt<NetworkWidget>());
+    menu->addItem("Accounts",     make_wt<AccountWidget>());
 
     // menu on left, selected menu item content on right
     hbox->addWidget(std::move(menu_container));
